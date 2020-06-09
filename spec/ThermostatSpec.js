b22748986b9ff10f3fbc663e5f40ed549d561827
victorvallet <victorvallet8@gmail.com>
2020-06-09 16:06:20 +0200

    Passed 2nd test for #increase function

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,14 +1,21 @@
-describe('Thermostat', function() {
+describe('Thermostat', function () {
   var thermostat;
-  beforeEach(function() {
+  beforeEach(function () {
     thermostat = new Thermostat();
   });
 
-describe('starting degrees', function(){
-  it('equal to 20', function() {
-    expect(thermostat.degrees).toEqual(20);
+  describe('starting degrees', function () {
+    it('equal to 20', function () {
+      expect(thermostat.degrees).toEqual(20);
+    });
   });
-});
+
+  describe('increase degrees', function () {
+    it('increases the temperature', function () {
+      thermostat.increase(15)
+      expect(thermostat.degrees).toEqual(35);
+    });
+  });
 
 
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -2,4 +2,8 @@ class Thermostat {
   constructor() {
     this.degrees = 20
   }
+
+  increase(num) {
+    this.degrees += num;
+  }
 };
